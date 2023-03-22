730b6b46d50aedea37c8a4f6f904ec62bf1a7654
dfernando2 <127353392+dfernando2@users.noreply.github.com>
2023-03-22 19:07:21 -0300

    actualisar ejericios Extras

--- Guia5/src/guia5/ejercicioExtra1.java
+++ Guia5/src/guia5/ejercicioExtra1.java
@@ -14,6 +14,27 @@ public class ejercicioExtra1 {
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingresar la cantidad de numeros para calcular");
+        int n = leer.nextInt();
+        float suma = 0;
+        
+        float[] vector = new float[n];
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("Ingrese el numero:");
+
+            vector[i] = leer.nextFloat();
+            
+            suma = suma + vector[i];
+        }
+        
+        System.out.println("La suma de los numeros es igual a: " + suma);
+        
+        
+        
+        
     }
 
 }
