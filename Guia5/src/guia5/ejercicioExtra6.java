901883a04b9779fa671942968d45b3bc18141d27
Dario Matorra <127353392+dfernando2@users.noreply.github.com>
2023-03-23 23:40:21 -0300

    encuentro 12

--- Guia5/src/guia5/ejercicioExtra6.java
+++ Guia5/src/guia5/ejercicioExtra6.java
@@ -13,7 +13,6 @@ package guia5;
 
 import java.util.Random;
 import java.util.Scanner;
-import static jdk.nashorn.internal.objects.NativeString.substring;
 
 /**
  *
@@ -25,6 +24,7 @@ public class ejercicioExtra6 {
 
         Scanner leer = new Scanner(System.in);
         Random random = new Random();
+        int dimension = 20;
 
         int[] vectorAleat1 = new int[5];
 
@@ -33,35 +33,37 @@ public class ejercicioExtra6 {
         }
 
         String[][] sopa = new String[20][20];
-        int cont = 0;
+
+        iniciar(sopa, dimension);
+
+        int cont = 1;
 
         do {
 
-            System.out.println("Ingrese palabras de 3 a 5 caracteres:");
-            String palabra = leer.nextLine();
-            int aux = 1;
             for (int i = 0; i < 20; i++) {
-                int aleatorio = random.nextInt(15);
-                for (int j = 0; j < 20; j++) {
-                    aux=1;
-                    if (i == vectorAleat1[0] || i == vectorAleat1[1] || i == vectorAleat1[2] || i == vectorAleat1[3] || i == vectorAleat1[4]) {
+                int aux = 1;
+                
+                int aleatorio = random.nextInt(20 - palabra.length());
+
+                if (i == vectorAleat1[0] || i == vectorAleat1[1] || i == vectorAleat1[2] || i == vectorAleat1[3] || i == vectorAleat1[4]) {
+                    String palabra = ingresarPalabra(aux);
+                    aux++;
+                    for (int j = 0; j < 20; j++) {
 
                         if (j >= aleatorio && j < (aleatorio + palabra.length())) {
-                            sopa[i][j] = palabra.substring(j - aleatorio, ((j - aleatorio) + aux));
-                            aux++;
 
-                        } else {
-                            sopa[i][j] = String.valueOf(random.nextInt(9));
+                            sopa[i][j] = palabra.substring(j - aleatorio, ((j - aleatorio) + 1));
+
                         }
-                    } else {
-                        sopa[i][j] = String.valueOf(random.nextInt(9));
-                    }
-                }
 
+                    }
+
+                }
             }
             cont++;
+        } while (cont < 6);
 
-        } while (cont < 5);
+        completar(sopa, dimension);
 
         for (int i = 0; i < sopa.length; i++) {
             for (int j = 0; j < sopa.length; j++) {
@@ -71,12 +73,42 @@ public class ejercicioExtra6 {
         }
     }
 
+    /*------------------------------------------------------------------------------
+    
+     */
+    public static String ingresarPalabra(int cont) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese palabras de 3 a 5 caracteres " + cont + ":");
+        String palabra = leer.nextLine();
+        if (palabra.length() > 5 || palabra.length() < 3) {
+            System.out.println("Ingrese palabras de 3 a 5 caracteres " + cont + ":");
+            palabra = leer.nextLine();
+        }
+        return palabra;
+    }
+
+    public static void iniciar(String sopa[][], int num) {
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                sopa[i][j] = "*";
+            }
+
+        }
+    }
+
+    public static void completar(String sopa[][], int num) {
+        Random random = new Random();
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                if (sopa[i][j] == "*") {
+                    sopa[i][j] = String.valueOf(random.nextInt(9));
+                }
+
+            }
+        }
+    }
 }
-//        String palabra1 = leer.nextLine();
-//        String palabra2 = leer.nextLine();
-//        String palabra3 = leer.nextLine();
-//        String palabra4 = leer.nextLine();
-//        String palabra5 = leer.nextLine();
+
 //        String[] vector1 = new String[palabra1.length()];
 //        String[] vector2 = new String[palabra2.length()];
 //        String[] vector3 = new String[palabra3.length()];
--- Guia5/src/guia5/ejericicioExtra6b.java
+++ Guia5/src/guia5/ejericicioExtra6b.java
@@ -20,40 +20,134 @@ public class ejericicioExtra6b{
     
     public static void main(String[] args) {
          
-        Scanner input = new Scanner(System.in);
-               
-        String[] palabras = new String[5];
+       Scanner leer = new Scanner(System.in);
+        String palabra = "";
+        boolean ref1;
+        String[][] sopa = new String[20][20];
+        int[] posicionH1 = new int[5];
+        int[] posicionV1 = new int[5];
+        int a=5;
+        inicializoVector(posicionH1, a);
+        inicializoVector(posicionV1, a);
+        
+        inicializoSopa(sopa);
+
         for (int i = 0; i < 5; i++) {
-            System.out.print("Ingrese una palabra de 3 a 5 caracteres: ");
-            palabras[i] = input.nextLine();
-            while (palabras[i].length() < 3 || palabras[i].length() > 5) {
-                System.out.print("Error. Ingrese una palabra de 3 a 5 caracteres: ");
-                palabras[i] = input.nextLine();
-            }
+            ref1 = true;
+            do {
+                if (ref1) {
+                    System.out.println("Ingrese una palabra Nro " + (i + 1));
+                } else {
+                    System.out.println("vuelva ingresar la palabra Nro " + (i + 1));
+                }
+
+                palabra = leer.next();
+                ref1 = false;
+            } while ((palabra.length() < 3) || (palabra.length() > 5));
+            ingresoASopa(sopa, palabra, posicionH1, posicionV1);
         }
+       
+        rellenoVacios(sopa);
+        imprimoSopa(sopa);
+
+    }
+
+    public static void ingresoASopa(String[][] sopa, String palabra, int[] posicionH1, int[] posicionV1){
+
+        int posicionH = buscoPosicionH(posicionH1);
+        int posicionV = buscoPosicionV(posicionV1, palabra);
+
+        int largo = palabra.length();
+
+        for (int i = 0; i < largo; i++) {
+            sopa[posicionH][posicionV + i] = palabra.toLowerCase().substring(i, i + 1);
+        }
+
+    }
+
+    public static int buscoPosicionH(int[] posicionH1) {
+
+        Random random = new Random();
+        
+        boolean ref2 = false;
+        int pos;
+
+        do {
+
+            pos = random.nextInt(20);
+
+            for (int i = 0; i < 5; i++) {
                
-        char[][] sopa = new char[20][20];
-        Random rand = new Random();
-        int fila = rand.nextInt(20);
-        int columna = rand.nextInt(16);
-        int palabraSeleccionada = rand.nextInt(5);
-        String palabra = palabras[palabraSeleccionada];
-        for (int i = 0; i < palabra.length(); i++) {
-            sopa[fila][columna+i] = palabra.charAt(i);
-        }
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                if (sopa[i][j] == 0) {
-                    sopa[i][j] = (char) (rand.nextInt(10) + '0');
+                if (posicionH1[i] == 100 ) {
+                    posicionH1[i] = pos;
+                    ref2 = false;
+                    break;
+                } else {
+                    if (posicionH1[i]==pos) {
+                        ref2 = true;
+                        break;
+                    }
+                    
                 }
             }
-        }
-                
+        } while (ref2);
+
+        return pos;
+
+    }
+
+    public static int buscoPosicionV(int[] posicionV1, String palabra) {
+
+        Random random = new Random();
+        
+       
+        int pos;
+        int rand=(20-palabra.length());
+      
+            pos = random.nextInt(rand);
+
+        return pos;
+    }
+
+    public static void imprimoSopa(String[][] sopa) {
+
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 20; j++) {
                 System.out.print(sopa[i][j] + " ");
             }
-            System.out.println();
+            System.out.println("");
+        }
+    }
+
+    public static void rellenoVacios(String[][] sopa) {
+
+        Random random = new Random();
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (sopa[i][j].equals("0")) {
+                    int relleno = random.nextInt(9);
+                    String numCadena = Integer.toString(relleno);
+                    sopa[i][j] = numCadena;
+                }
+            }
+        }
+    }
+    public static void inicializoSopa(String[][] sopa){
+        
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                sopa[i][j]="0";
+            }
+        }
+        
+        
+    }
+
+    public static void inicializoVector(int[] vector,int a){
+        
+        for (int i = 0; i < a; i++) {
+            vector[i]=100;
         }
     }
 }
