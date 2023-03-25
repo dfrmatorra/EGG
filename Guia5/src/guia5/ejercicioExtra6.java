83812e9a2d1fc89791528fe98f6ed2288097dc42
Dario Matorra <127353392+dfernando2@users.noreply.github.com>
2023-03-25 13:52:36 -0300

    EjercicioExtra6 mejorado

--- Guia5/src/guia5/ejercicioExtra6.java
+++ Guia5/src/guia5/ejercicioExtra6.java
@@ -13,10 +13,8 @@ package guia5;
 
 import java.util.Random;
 import java.util.Scanner;
-
-/**
- *
- * @author Dario
+/*
+ @author Dario
  */
 public class ejercicioExtra6 {
 
@@ -24,35 +22,37 @@ public class ejercicioExtra6 {
 
         Scanner leer = new Scanner(System.in);
         Random random = new Random();
-        int dimension = 20;
-        int cantidadPalabras = 5;
-
-        String[][] sopa = new String[20][20];
-
+        int dimension = 30;//dimension de la sopa de letras
+        int cantidadPalabras = 5; //cantidad de palabras para incorporar
+        
+//crear e inicializar matriz sopa
+        String[][] sopa = new String[dimension][dimension];
         iniciar(sopa, dimension);
-//buclecompleta el vector palabra para poder completar con las condiciones solicitadas
-        String palabra[] = new String[5];
-        for (int i = 0; i < 5; i++) {
+        
+//bucle completa el vector palabra para poder completar con las condiciones solicitadas
+        String palabra[] = new String[cantidadPalabras];
+
+        for (int i = 0; i < cantidadPalabras; i++) {
             System.out.println("Ingrese una palabra de 3 a 5 caracteres (" + (i + 1) + "):");
             palabra[i] = leer.nextLine();
             while (palabra[i].length() > 5 || palabra[i].length() < 3) {
                 System.out.println("Ingrese una palabra de 3 a 5 caracteres (" + (i + 1) + "):");
                 palabra[i] = leer.nextLine();
             }
-        }
+        } 
+
 //generar 2 vectores aleatorios
         int[] vectorAleat1 = new int[cantidadPalabras]; //5 numeros del 0 al 20
         aleatorio1(cantidadPalabras, vectorAleat1);//completa el vector vectorAleat1[cont]
-        int[] vectorAleat2 = new int[cantidadPalabras];//5 numeros del 0 hasta 20-el largo de la palabra, para que se salga de la columna 20
-        aleatorio2(cantidadPalabras, palabra, vectorAleat2);//completa el vector vectorAleat2[cont]
+        int[] vectorAleat2 = new int[cantidadPalabras];//cantidad de numeros del 0 hasta 20-el largo de la palabra, para que se salga de la columna 20
+        aleatorio2(cantidadPalabras, dimension, palabra, vectorAleat2);//completa el vector vectorAleat2[cont]
 
 //completo matriz sopa con las palabras ingresadas en forma aleatoria
         int cont = 0;
         do {
-
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < dimension; i++) {
                 if (i == vectorAleat1[cont]) {
-                    for (int j = 0; j < 20; j++) {
+                    for (int j = 0; j < dimension; j++) {
                         if ((j >= vectorAleat2[cont]) && (j < (vectorAleat2[cont] + (palabra[cont].length())))) {
                             sopa[i][j] = palabra[cont].substring(j - vectorAleat2[cont], (j - vectorAleat2[cont]) + 1);
                         }
@@ -60,9 +60,11 @@ public class ejercicioExtra6 {
                 }
             }
             cont++;
-        } while (cont < 5);//bucle hasta completar las 5 palabras
+        } while (cont < cantidadPalabras);//bucle hasta completar las 5 palabras
+
 //completo con numeros del 0 al 9 en forma aleatoria
         completar(sopa, dimension);
+
 //muestro la matriz sopa con palabras y numero mezclados aleatoriamente
         for (int i = 0; i < sopa.length; i++) {
             for (int j = 0; j < sopa.length; j++) {
@@ -72,16 +74,13 @@ public class ejercicioExtra6 {
         }
     }
     //--------------------------------------------------------------------------//
-
     public static void iniciar(String sopa[][], int num) {
         for (int i = 0; i < num; i++) {
             for (int j = 0; j < num; j++) {
                 sopa[i][j] = "*";
             }
-
         }
     }
-
     public static void completar(String sopa[][], int num) {
         Random random = new Random();
         for (int i = 0; i < num; i++) {
@@ -89,11 +88,9 @@ public class ejercicioExtra6 {
                 if (sopa[i][j] == "*") {
                     sopa[i][j] = String.valueOf(random.nextInt(9));
                 }
-
             }
         }
     }
-
     public static void aleatorio1(int num, int vectorAleat[]) {
         Random random = new Random();
         int cont = 0;
@@ -107,20 +104,13 @@ public class ejercicioExtra6 {
             }
             cont++;
         }
-
     }
-
-    public static void aleatorio2(int num, String palabra[], int vectorAleat[]) {
+    public static void aleatorio2(int num, int dimension, String palabra[], int vectorAleat2[]) {
         Random random = new Random();
         for (int i = 0; i < num; i++) {
-            vectorAleat[i] = random.nextInt(20 - palabra[i].length());
+            vectorAleat2[i] = random.nextInt(dimension - palabra[i].length());
         }
 
     }
 }
 
-//        String[] vector1 = new String[palabra1.length()];
-//        String[] vector2 = new String[palabra2.length()];
-//        String[] vector3 = new String[palabra3.length()];
-//        String[] vector4 = new String[palabra4.length()];
-//        String[] vector5 = new String[palabra5.length()];
